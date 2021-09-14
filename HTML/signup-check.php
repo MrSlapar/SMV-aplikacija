a568cdff5e3dd139859af09848b457b1539d4c85
TriGaj <tristangajsek69@gmail.com>
2021-09-14 11:16:57 +0200

    Signup končno dela

--- HTML/signup-check.php
+++ HTML/signup-check.php
@@ -9,8 +9,8 @@ $conn->query("USE Eucilnica");
 $result = $conn->query("
 	INSERT INTO Dijaki(ime, priimek, password) VALUES
 	(" .
-		"'" . $_POST["ime"] . "', " .
-		"'" . $_POST["priimek"] . "', " .
+		"'" . $_POST["name"] . "', " .
+		"'" . $_POST["surname"] . "', " .
 		"'" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "'
 	);"
 );
