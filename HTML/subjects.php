5fe0050e4c5fe201be9b18e1b4f18978f388d73f
TriGaj <90246264+TriGaj@users.noreply.github.com>
2021-10-19 08:13:39 +0200

    idk

---
+++ HTML/subjects.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<title>
+		</title>
+		<link rel="stylesheet" href="../CSS/header.css">
+		<link rel="stylesheet" href="../CSS/loggedIndex.css">
+	</head>
+	<body>
+		<div class = "header">
+			<div class = "item" id = "homeHeader">
+				<span class = "headerText">
+					HOME
+				</span>
+			</div>
+			<div class = "item" id= "subjectsHeader">
+				<span class = "headerText">
+					SUBJECTS
+				</span>
+			</div>
+			<div class = "item" id= "professorsHeader">
+				<span class = "headerText">
+					PROFESSORS
+				</span>
+			</div>
+			<div class = "item" id= "assignmentsHeader">
+				<span class = "headerText">
+					ASSIGNMENTS
+				</span>
+			</div>
+			<div class = "item" id= "loginHeader">
+				<span class = "headerText">
+					LOG OUT
+				</span>
+			</div>
+		</div>
+		<div class = "notifications">
+			<span id = "notificationTitle">
+				SUBJECTS
+			</span>
+			<div> <!-- array -->
+				<span>
+					Assignment 1
+				</span>
+			</div>
+		</div>
+		<div class = "main">
+			<div class = "subMain">
+				<img src = "../images/scc/background.jpg" id = "mainImage">
+			</div>
+		</div>
+	</body>
+</html>
