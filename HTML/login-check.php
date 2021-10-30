2907ec66b19c44874a42c101c6d68d5d2202fda6
TriGaj <90246264+TriGaj@users.noreply.github.com>
2021-10-30 13:45:07 +0200

    login dela (sam za dijake za zej)

--- HTML/login-check.php
+++ HTML/login-check.php
@@ -5,4 +5,16 @@ $password = "";
 
 $conn = mysqli_connect($servername, $username, $password);
 $conn->query("USE Eucilnica");
+
+$result = $conn->query("SELECT password FROM Dijaki WHERE ime = '" . $_POST["name"] . "' AND priimek = '" . $_POST["surname"] . "'");
+if($result !== false && $result->num_rows > 0){
+	$row = $result->fetch_assoc();
+	if(password_verify($_POST["password"], $row["password"])){
+		header("Location: index.php?id=" . $row["id"]);
+	}else{
+		header("Location: login.php");
+	}
+}else{
+	header("Location: login.php");
+}
 ?>
