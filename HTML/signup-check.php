aac3d7f62910e450684681e097d7bf32edfefbe4
TriGaj <90246264+TriGaj@users.noreply.github.com>
2021-10-30 14:01:57 +0200

    signup ne dela če ne daš vseh podatkov

--- HTML/signup-check.php
+++ HTML/signup-check.php
@@ -6,12 +6,18 @@ $password = "";
 $conn = mysqli_connect($servername, $username, $password);
 $conn->query("USE Eucilnica");
 
-$result = $conn->query("
-	INSERT INTO Dijaki(ime, priimek, password) VALUES
-	(" .
-		"'" . $_POST["name"] . "', " .
-		"'" . $_POST["surname"] . "', " .
-		"'" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "'
-	);"
-);
+if($_POST["name"] != "" && $_POST["surname"] != "" && $_POST["password"] != ""){
+	$result = $conn->query("
+		INSERT INTO Dijaki(ime, priimek, password) VALUES
+		(" .
+			"'" . $_POST["name"] . "', " .
+			"'" . $_POST["surname"] . "', " .
+			"'" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "'
+		);"
+	);
+	
+	header("Location: index.php");
+}else{
+	header("Location: signup.php");	
+}
 ?>
